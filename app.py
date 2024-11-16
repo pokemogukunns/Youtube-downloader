d5c0844557ff2364ab8e610af62071f0354c3a5a
pokemogukunns <nattyannkunndesu@icloud.com>
2024-11-16 09:17:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 from flask import Flask
-from flask_cors import CORS
-from get_link import get_download_link
+from get_link import get_download_link  # Blueprintのインポート
 from download import download_mp3, download_mp4
+from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)  # CORSを許可
 
-# Blueprintを登録
+# YouTube動画の情報とダウンロードリンクを取得するエンドポイント
 app.register_blueprint(get_download_link)
+
+# 動画のMP3をダウンロードするエンドポイント
 app.register_blueprint(download_mp3)
+
+# 動画のMP4をダウンロードするエンドポイント
 app.register_blueprint(download_mp4)
 
 if __name__ == '__main__':
